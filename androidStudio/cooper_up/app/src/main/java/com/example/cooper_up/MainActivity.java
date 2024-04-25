c1f2c4ccb41d189ab7b4db7e8793d3e6413b264a
carloosbravo <carloosbravo04@gmail.com>
2024-04-25 09:06:07 +0200

    nav menu en proceso

--- androidStudio/cooper_up/app/src/main/java/com/example/cooper_up/MainActivity.java
+++ androidStudio/cooper_up/app/src/main/java/com/example/cooper_up/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.cooper_up;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private BottomNavigationView bottomNavigationView;
+    private FrameLayout frameLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavView);
+        frameLayout = findViewById(R.id.frameLayout);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.navHome) {
+
+                    loadFragment(new HomeFragment(), true);
+
+                }  else if (itemId == R.id.navAdd){ // nav Add
+
+                    loadFragment(new AddFragment(), false);
+
+                }
+                else {
+
+                    loadFragment(new SearchFragment(), false);
+
+                }
+
+                return true;
+            }
+        });
     }
 }
---
+++ androidStudio/cooper_up/app/src/main/java/com/example/cooper_up/SelectorUsuario.java
@@ -0,0 +1,14 @@
+package com.example.cooper_up;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectorUsuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selector_usuario);
+    }
+}
