86151ed5481066b0bffe7a812ff85f33b1ff5108
carloosbravo <carloosbravo04@gmail.com>
2024-04-25 12:05:56 +0200

    fragments creados

--- androidStudio/cooper_up/app/src/main/java/com/example/cooper_up/MainActivity.java
+++ androidStudio/cooper_up/app/src/main/java/com/example/cooper_up/MainActivity.java
@@ -2,14 +2,17 @@ package com.example.cooper_up;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
 
+import com.example.cooper_up.fragments.HomeFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     private BottomNavigationView bottomNavigationView;
     private FrameLayout frameLayout;
@@ -17,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         bottomNavigationView = findViewById(R.id.bottomNavView);
         frameLayout = findViewById(R.id.frameLayout);
@@ -31,7 +35,11 @@ public class MainActivity extends AppCompatActivity {
 
                     loadFragment(new HomeFragment(), true);
 
-                }  else if (itemId == R.id.navAdd){ // nav Add
+                } else if (itemId == R.id.navProfile) {
+
+                    loadFragment(new ProfileFragment(), false);
+
+                } else if (itemId == R.id.navAdd){ // nav Add
 
                     loadFragment(new AddFragment(), false);
 
@@ -45,5 +53,20 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+
+    }
+
+    private void loadFragment(Fragment fragment, boolean isAppInitialized) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        if(isAppInitialized){
+            fragmentTransaction.add(R.id.frameLayout, fragment);
+        } else {
+            fragmentTransaction.replace(R.id.frameLayout, fragment);
+        }
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }
