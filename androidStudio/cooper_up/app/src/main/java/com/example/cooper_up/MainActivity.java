31e6310f7abd74d1f3b6eb00dae0c1297e2a19aa
carloosbravo <carloosbravo04@gmail.com>
2024-05-09 09:57:11 +0200

    diseño de cosa varias

--- androidStudio/cooper_up/app/src/main/java/com/example/cooper_up/MainActivity.java
+++ androidStudio/cooper_up/app/src/main/java/com/example/cooper_up/MainActivity.java
@@ -3,70 +3,73 @@ package com.example.cooper_up;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
+import com.example.cooper_up.fragments.AddFragment;
 import com.example.cooper_up.fragments.HomeFragment;
+import com.example.cooper_up.fragments.ProfileFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-
     private BottomNavigationView bottomNavigationView;
     private FrameLayout frameLayout;
+
+    // Fragmentos
+    private HomeFragment homeFragment;
+    private AddFragment addFragment;
+    private ProfileFragment profileFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         bottomNavigationView = findViewById(R.id.bottomNavView);
         frameLayout = findViewById(R.id.frameLayout);
 
-        //bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            //@Override
-            //public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Inicializar fragmentos
+        homeFragment = new HomeFragment();
+        addFragment = new AddFragment();
+        profileFragment = new ProfileFragment();
 
-                //int itemId = item.getItemId();
-//
-                //if (itemId == R.id.navHome) {
-//
-                //    loadFragment(new HomeFragment(), true);
-//
-                //} else if (itemId == R.id.navProfile) {
-//
-                //    loadFragment(new ProfileFragment(), false);
-//
-                //} else if (itemId == R.id.navAdd){ // nav Add
-//
-                //    loadFragment(new AddFragment(), false);
-//
-                //}
-                //else {
-//
-                //    loadFragment(new SearchFragment(), false);
-//
-                //}
-//
-                //return true;
-       //    }
-       //});
+        // Establecer el fragmento inicial
+        setFragment(homeFragment);
 
+        // Listener para el BottomNavigationView
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.navHome) {
+                    Toast.makeText(MainActivity.this, "home", Toast.LENGTH_SHORT).show();
+                    setFragment(homeFragment);
+                    return true;
+                } else if (itemId == R.id.navAdd) {
+                    setFragment(addFragment);
+                    return true;
+                } else if (itemId == R.id.navProfile) {
+                    Toast.makeText(MainActivity.this, "profile", Toast.LENGTH_SHORT).show();
+                    setFragment(profileFragment);
+                    return true;
+                } else {
+                    return false;
+                }
+
+            }
+        });
     }
 
-   //private void loadFragment(Fragment fragment, boolean isAppInitialized) {
-
-   //    FragmentManager fragmentManager = getSupportFragmentManager();
-   //    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-
-   //    if(isAppInitialized){
-   //        fragmentTransaction.add(R.id.frameLayout, fragment);
-   //    } else {
-   //        fragmentTransaction.replace(R.id.frameLayout, fragment);
-   //    }
-   //    fragmentTransaction.replace(R.id.frameLayout, fragment);
-   //    fragmentTransaction.commit();
-   //}
+    // Método para cambiar el fragmento
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
+    }
 }
---
+++ androidStudio/cooper_up/app/src/main/java/com/example/cooper_up/adapters/RVhome.java
@@ -0,0 +1,5 @@
+package com.example.cooper_up.adapters;
+
+public class RVhome {
+
+}
