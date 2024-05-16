5993da9e19fe3dd6d5a3e2f66054ed66972f04e4
carloosbravo <carloosbravo04@gmail.com>
2024-05-16 09:58:11 +0200

    clase Practica creada

---
+++ androidStudio/cooper_up/app/src/main/java/com/example/cooper_up/PracticaPulsar.java
@@ -0,0 +1,14 @@
+package com.example.cooper_up;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PracticaPulsar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practica_pulsar);
+    }
+}
