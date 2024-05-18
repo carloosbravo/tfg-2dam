8293378abcb0de11fcc7b34d3fd56adee1389d42
carloosbravo <carloosbravo04@gmail.com>
2024-05-18 13:40:31 +0200

    boton editar usuario funciona

--- androidStudio/cooper_up/app/src/main/java/com/example/cooper_up/EditarUsuario.java
+++ androidStudio/cooper_up/app/src/main/java/com/example/cooper_up/EditarUsuario.java
@@ -1,8 +1,12 @@
 package com.example.cooper_up;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.example.cooper_up.fragments.ProfileFragment;
 
 public class EditarUsuario extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class EditarUsuario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editar_usuario);
+
+        ImageButton volverPerfilButton = findViewById(R.id.volverPerfilButton);
+        volverPerfilButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(EditarUsuario.this, MainActivity.class);  // Reemplaza 'ProfileActivity' con la actividad a la que quieres ir
+                startActivity(intent);
+            }
+        });
     }
 }
--- androidStudio/cooper_up/app/src/main/java/com/example/cooper_up/fragments/ProfileFragment.java
+++ androidStudio/cooper_up/app/src/main/java/com/example/cooper_up/fragments/ProfileFragment.java
@@ -1,28 +1,35 @@
 package com.example.cooper_up.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import com.example.cooper_up.EditarUsuario;
 import com.example.cooper_up.R;
-
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import androidx.fragment.app.Fragment;
 
 public class ProfileFragment extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        FloatingActionButton editarPerfilButton = view.findViewById(R.id.editarPerfilButton);
+        //cuando se haga click en el boton se cambia a ala actividad de editar usuario
+
+        editarPerfilButton.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), EditarUsuario.class);
+            startActivity(intent);
+        });
+
+        return view;
     }
 }
