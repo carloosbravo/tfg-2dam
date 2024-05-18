61051d4317f48a53c129ab40e462e9ef9b62d00c
carloosbravo <carloosbravo04@gmail.com>
2024-05-18 17:32:56 +0200

    diseño activity practica cuando se pulsa hecho

--- androidStudio/cooper_up/app/src/main/java/com/example/cooper_up/PracticaPulsar.java
+++ androidStudio/cooper_up/app/src/main/java/com/example/cooper_up/PracticaPulsar.java
@@ -3,6 +3,7 @@ package com.example.cooper_up;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class PracticaPulsar extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class PracticaPulsar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practica_pulsar);
+
+        //pulsar para volver al home
+        ImageButton volverHomeButton = findViewById(R.id.volverHomeTV);
     }
 }
