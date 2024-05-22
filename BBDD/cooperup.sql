e18e8c0effd7741c6ce329eceb80ad835d3c503e
gonzarf <gonzarf21@gmail.com>
2024-05-22 13:34:14 +0200

    bbdd modificada

--- BBDD/cooperup.sql
+++ BBDD/cooperup.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 02-05-2024 a las 14:38:24
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
+-- Tiempo de generación: 22-05-2024 a las 13:33:40
+-- Versión del servidor: 10.4.11-MariaDB
+-- Versión de PHP: 7.4.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -25,19 +26,8 @@ SET time_zone = "+00:00";
 
 --
 -- Estructura de tabla para la tabla `centro`
-<<<<<<< HEAD:BBDD/cooperup.sql
 --
-=======
-<<<<<<< Updated upstream:BBDD/coperUP.sql
-CREATE database cooperup;
-USE cooperup;
->>>>>>> Spring:BBDD/coperUP.sql
 
-=======
---
-CREATE DATABASE cooperapp;
-use cooperapp;
->>>>>>> Stashed changes:BBDD/cooperup.sql
 CREATE TABLE `centro` (
   `id` int(11) NOT NULL,
   `nombre` varchar(255) NOT NULL,
@@ -45,18 +35,44 @@ CREATE TABLE `centro` (
   `direccion` varchar(255) DEFAULT NULL,
   `contraseña` varchar(255) NOT NULL,
   `telefono` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `centro`
 --
 
 INSERT INTO `centro` (`id`, `nombre`, `email`, `direccion`, `contraseña`, `telefono`) VALUES
-(1, 'Centro1', 'centro1@example.com', 'Dirección Centro 1', 'contraseña1', '123456789'),
-(2, 'Centro2', 'centro2@example.com', 'Dirección Centro 2', 'contraseña2', '987654321'),
-(3, 'Centro3', 'centro3@example.com', 'Dirección Centro 3', 'contraseña3', '111111111'),
-(4, 'Centro4', 'centro4@example.com', 'Dirección Centro 4', 'contraseña4', '222222222'),
-(5, 'Centro5', 'centro5@example.com', 'Dirección Centro 5', 'contraseña5', '333333333');
+(1, 'Centro1', 'centro1@example.com', 'Dirección Centro 1', 'contrasena1', '123456789'),
+(2, 'Centro2', 'centro2@example.com', 'Dirección Centro 2', 'contrasena2', '987654321'),
+(3, 'Centro3', 'centro3@example.com', 'Dirección Centro 3', 'contrasena3', '111111111'),
+(4, 'Centro4', 'centro4@example.com', 'Dirección Centro 4', 'contrasena4', '222222222'),
+(5, 'Centro5', 'centro5@example.com', 'Dirección Centro 5', 'contrasena5', '333333333'),
+(6, 'IFP', 'ifp@ifp.com', 'Calle Alcalá', 'contrasenaIFP', '111111111');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cv`
+--
+
+CREATE TABLE `cv` (
+  `descripcion` varchar(100) NOT NULL,
+  `estudios` varchar(100) NOT NULL,
+  `lenguajes` varchar(1000) NOT NULL,
+  `id_estudiante` int(11) NOT NULL,
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `cv`
+--
+
+INSERT INTO `cv` (`descripcion`, `estudios`, `lenguajes`, `id_estudiante`, `id`) VALUES
+('Desarrollador con experiencia en aplicaciones web', 'Ingeniería Informática', 'JavaScript, HTML, CSS, Python', 1, 1),
+('Analista de datos con habilidades en big data', 'Estadística', 'R, Python, SQL', 2, 2),
+('Especialista en marketing digital', 'Marketing', 'SEO, SEM, Google Analytics', 3, 3),
+('Diseñador gráfico con un ojo para los detalles', 'Diseño Gráfico', 'Photoshop, Illustrator, InDesign', 4, 4),
+('Experto en ciberseguridad con conocimientos avanzados', 'Ciberseguridad', 'Penetration Testing, Firewalls, Network Security', 5, 5);
 
 -- --------------------------------------------------------
 
@@ -71,10 +87,20 @@ CREATE TABLE `empresa` (
   `contraseña` varchar(255) NOT NULL,
   `direccion` varchar(255) DEFAULT NULL,
   `telefono` varchar(20) DEFAULT NULL,
-  `biografia` text DEFAULT NULL,
-  `oferta_practicas` text DEFAULT NULL,
-  `oferta_trabajo` text DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `biografia` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `empresa`
+--
+
+INSERT INTO `empresa` (`id`, `nombre`, `email`, `contraseña`, `direccion`, `telefono`, `biografia`) VALUES
+(1, 'Empresa1', 'empresa1@example.com', 'contrasena1', 'Dirección Empresa 1', '123456789', 'Biografía Empresa 1'),
+(2, 'Empresa2', 'empresa2@example.com', 'contrasena2', 'Dirección Empresa 2', '987654321', 'Biografía Empresa 2'),
+(3, 'Empresa3', 'empresa3@example.com', 'contrasena3', 'Dirección Empresa 3', '555555555', 'Biografía Empresa 3'),
+(4, 'Empresa4', 'empresa4@example.com', 'contrasena4', 'Dirección Empresa 4', '111111111', 'Biografía Empresa 4'),
+(5, 'Empresa5', 'empresa5@example.com', 'contrasena5', 'Dirección Empresa 5', '999999999', 'Biografía Empresa 5'),
+(6, 'EVIDEN', 'eviden@example.com', 'contrasenaEviden', 'Dirección Eviden 4', '989898989898', 'Biografía Eviden 4');
 
 -- --------------------------------------------------------
 
@@ -87,7 +113,18 @@ CREATE TABLE `oferta` (
   `estado` tinyint(1) DEFAULT NULL,
   `id_practica` int(11) NOT NULL,
   `id_estudiante` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `oferta`
+--
+
+INSERT INTO `oferta` (`id`, `estado`, `id_practica`, `id_estudiante`) VALUES
+(1, 1, 1, 1),
+(2, 1, 2, 2),
+(3, 0, 3, 3),
+(4, 1, 4, 4),
+(5, 0, 5, 5);
 
 -- --------------------------------------------------------
 
@@ -98,8 +135,20 @@ CREATE TABLE `oferta` (
 CREATE TABLE `practica` (
   `id` int(11) NOT NULL,
   `descripcion` varchar(200) NOT NULL,
-  `id_empresa` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `id_empresa` int(11) NOT NULL,
+  `titulo` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `practica`
+--
+
+INSERT INTO `practica` (`id`, `descripcion`, `id_empresa`, `titulo`) VALUES
+(1, 'Práctica en desarrollo web', 1, 'Desarrollo Web'),
+(2, 'Práctica en análisis de datos', 1, 'Análisis de Datos'),
+(3, 'Práctica en marketing digital', 3, 'Marketing Digital'),
+(4, 'Práctica en diseño gráfico', 4, 'Diseño Gráfico'),
+(5, 'Práctica en ciberseguridad', 5, 'Ciberseguridad');
 
 -- --------------------------------------------------------
 
@@ -112,22 +161,23 @@ CREATE TABLE `usuarionormal` (
   `nombre` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `contraseña` varchar(255) NOT NULL,
-  `cv` text DEFAULT NULL,
   `expediente_academico` text DEFAULT NULL,
   `valoracion_profesorado` varchar(100) DEFAULT NULL,
-  `centro_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `centro_id` int(11) DEFAULT NULL,
+  `telefono` varchar(11) DEFAULT '""'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `usuarionormal`
 --
 
-INSERT INTO `usuarionormal` (`id`, `nombre`, `email`, `contraseña`, `cv`, `expediente_academico`, `valoracion_profesorado`, `centro_id`) VALUES
-(1, 'Usuario1', 'usuario1@example.com', 'contraseña1', 'CV Usuario 1', 'Expediente Usuario 1', 'Buena', 1),
-(2, 'Usuario2', 'usuario2@example.com', 'contraseña2', 'CV Usuario 2', 'Expediente Usuario 2', 'Excelente', 2),
-(3, 'Usuario3', 'usuario3@example.com', 'contraseña3', 'CV Usuario 3', 'Expediente Usuario 3', 'Muy Buena', 3),
-(4, 'Usuario4', 'usuario4@example.com', 'contraseña4', 'CV Usuario 4', 'Expediente Usuario 4', 'Regular', 4),
-(5, 'Usuario5', 'usuario5@example.com', 'contraseña5', 'CV Usuario 5', 'Expediente Usuario 5', 'Muy Mala', 5);
+INSERT INTO `usuarionormal` (`id`, `nombre`, `email`, `contraseña`, `expediente_academico`, `valoracion_profesorado`, `centro_id`, `telefono`) VALUES
+(1, 'diego', 'u', 'u', 'Expediente gonza', 'Malisimo', NULL, 'u'),
+(2, 'Usuario2', 'usuario2@example.com', 'contrasena2', 'Expediente Usuario 2', 'Excelente', 2, '75375373'),
+(3, 'Usuario3', 'usuario3@example.com', 'contrasena3', 'Expediente Usuario 3', 'Muy Buena', 3, '783783'),
+(4, 'Usuario4', 'usuario4@example.com', 'contrasena4', 'Expediente Usuario 4', 'Regular', 4, '3783783'),
+(5, 'Usuario5', 'usuario5@example.com', 'contrasena5', 'Expediente Usuario 5', 'Muy Mala', 5, '37837837'),
+(6, 'gonzalito', 'gonzalo@example.com', 'contrasena98', 'Expediente gonza', 'Malisimo', 2, '783783783');
 
 --
 -- Índices para tablas volcadas
@@ -138,6 +188,13 @@ INSERT INTO `usuarionormal` (`id`, `nombre`, `email`, `contraseña`, `cv`, `expe
 --
 ALTER TABLE `centro`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `cv`
+--
+ALTER TABLE `cv`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_estudiante` (`id_estudiante`);
 
 --
 -- Indices de la tabla `empresa`
@@ -168,8 +225,48 @@ ALTER TABLE `usuarionormal`
   ADD KEY `centro_id` (`centro_id`);
 
 --
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `centro`
+--
+ALTER TABLE `centro`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `cv`
+--
+ALTER TABLE `cv`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT de la tabla `empresa`
+--
+ALTER TABLE `empresa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `practica`
+--
+ALTER TABLE `practica`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarionormal`
+--
+ALTER TABLE `usuarionormal`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- Restricciones para tablas volcadas
 --
+
+--
+-- Filtros para la tabla `cv`
+--
+ALTER TABLE `cv`
+  ADD CONSTRAINT `cv_ibfk_1` FOREIGN KEY (`id_estudiante`) REFERENCES `usuarionormal` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `oferta`
