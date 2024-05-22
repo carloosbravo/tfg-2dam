9e95648bc4a78ae76798277f675f96c4b6443733
carloosbravo <carloosbravo04@gmail.com>
2024-05-22 13:37:22 +0200

    fragments empresa casi hechos

--- androidStudio/cooper_up/app/src/main/java/com/example/cooper_up/fragmentsEmpresa/HomeEmpresaFragment.java
+++ androidStudio/cooper_up/app/src/main/java/com/example/cooper_up/fragmentsEmpresa/HomeEmpresaFragment.java
@@ -2,15 +2,23 @@ package com.example.cooper_up.fragmentsEmpresa;
 
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.cooper_up.R;
+import com.example.cooper_up.adapters.RValumnos;
 import com.example.cooper_up.models.EmpresaModelo;
 
 public class HomeEmpresaFragment extends Fragment {
     private EmpresaModelo empresa;
+
+    private RecyclerView recyclerView;
+
+    private RValumnos adapter;
+    
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
